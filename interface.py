46a70691b3053e7aeae636f26d3ab83175bfd188
luislopes <luiiscsl95@gmail.com>
2026-08-20 23:06:36 -0300

    refactor: melhora nome da selecao

--- interface.py
+++ interface.py
@@ -714,10 +714,10 @@ def editar_selecionado():
         )
         return
 
-    item_selecionado = selecionado[0]
+    id_selecionado = selecionado[0]
 
     indice_lista = tabela_movimentacoes.index(
-        item_selecionado
+        id_selecionado
     )
 
     tipo, indice = movimentacoes[indice_lista]
